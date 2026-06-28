76b27cb0a04273b165b99c87a3fd89729d08e33e
Mohammad <waseem.razaa@gmail.com>
2026-06-28 22:19:34 +0200

    showcase spring ai basic chat using AWS Bedrock.

---
+++ 04-springai-aws-bedrock/src/test/java/com/learning/springai/AwsBedrockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsBedrockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
